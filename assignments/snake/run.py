207df0392fd4b169bb6d2265bab1d9091686f281
Kristoffer Solum <kristofso@uia.no>
2024-09-28 16:17:57 +0200

    Added changes to run.py

--- assignments/snake/run.py
+++ assignments/snake/run.py
@@ -1,10 +1,13 @@
-import random
+
 from snake import SnakeGame
+from snake_AI import create_path
 
 snake = SnakeGame()
 
+
 @snake.register_ai
 def super_ai():
-    return [random.choice(['up', 'down', 'left', 'right'])]
+    return create_path(snake)
+
 
 snake.start(use_ai=True)
